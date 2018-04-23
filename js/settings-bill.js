5489cd22410f438eda47e610401e50dd048bcf78
Candice <candice.m.sprout@gmail.com>
2018-04-23 16:21:48 +0200

    Functions adjustments

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,23 +1,62 @@
 //DOM ELEMENT REFERENCES:
-// SETTINGS INPUTS
-var callSet = document.querySelector('.callCostSetting');
-var smsSet = document.querySelector('.smsCostSetting');
-var warningSet = document.querySelector('.warningLevelSetting');
-var criticalSet = document.querySelector('.criticalLevelSetting');
+// SETTINGS INPUTS ELEMENTS
+var callSet = document.querySelector('.callCostSetting').value;
+  var updatedCall = parseFloat(callSet);
+var smsSet = document.querySelector('.smsCostSetting').value;
+  var updatedSms = parseFloat(smsSet);
+var warningSet = document.querySelector('.warningLevelSetting').value;
+  var updatedWarning = parseFloat(warningSet);
+var criticalSet = document.querySelector('.criticalLevelSetting').value;
+  var updatedCritical = parseFloat(criticalSet);
 
-//BUTTONS TO UPDATE / CALCULATE
+//BUTTONS ELEMENTS TO UPDATE / CALCULATE
 var settingsAdd = document.querySelector('.settingsAddBtn');
-var update = document.querySelector('.updateSettings');
+var updated = document.querySelector('.updateSettings');
 
-//TOTALS
+//TOTALS ELEMENTS
 var totalCalls = document.querySelector('.callTotalSettings');
 var totalSms = document.querySelector('.smsTotalSettings');
-var totalWithSettings = document.querySelector('.totalSettings')
+var totalWithSettings = document.querySelector('.totalSettings');
 
 //Global variables to store calculations from functions
 var callsWithSettings = 0;
 var smsWithSettings = 0;
-var totalTotal = 0;
+
+var callValue = 0;
+var smsValue = 0;
+
+updated.addEventListener('click', settings);
+
+
+function settings(){
+  if (updatedCall > 0) {
+   callValue = updatedCall;
+  }
+  if (updatedSms > 0) {
+   smsValue = updatedSms;
+  }
+}
+  function calculate() {
+
+    var selectedBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+    if (selectedBtn){
+        var item = selectedBtn.value
+      }
+
+  if (item === "call") {
+    callsWithSettings += callValue;
+  }
+  if (item === "sms") {
+    smsWithSettings += smsValue;
+  }
+  totalCalls.innerHTML = callsWithSettings.toFixed(2);
+  totalSms.innerHTML = smsWithSettings.toFixed(2);
+  var totalTotal = callsWithSettings + smsWithSettings;
+  totalWithSettings.innerHTML = totalTotal.toFixed(2);
+  }
+  settingsAdd.addEventListener('click', calculate);
+
+
 
 //add an event listener for when the 'Update settings' button is pressed
 
@@ -28,36 +67,3 @@ var totalTotal = 0;
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen.
 // * check the value thresholds and display the total value in the right color.
- function settings (){
-   var updateCall = parseFloat(callSet.value);
-   var updateSms = parseFloat(smsSet.value);
-   var updateWarning = parseFloat(warningSet.value);
-   var updateCritical = parseFloat(criticalSet.value);
-
-   var selectedBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-    if (selectedBtn){
-      var selectedValue = selectedBtn.value
-    }
-   function calculateSettings() {
-     if (selectedValue === "call") {
-       callsWithSettings += updateCall;
-     }
-     if (selectedValue === "sms") {
-       smsWithSettings += updateSms;
-     }
-     totalCalls.innerHTML = callsWithSettings.toFixed(2);
-     totalSms.innerHTML = smsWithSettings.toFixed(2);
-      totalTotal = callsWithSettings + smsWithSettings;
-     totalWithSettings.innerHTML = totalTotal.toFixed(2);
-   }
-
-   function thresholds() {
-     if (totalTotal) {
-
-     }
-
-   }
-
- }
- update.addEventListener('click', settings);
- settingsAdd.addEventListener('click', calculateSettings);
